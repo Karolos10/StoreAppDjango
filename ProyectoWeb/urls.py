d22cb11335ec6ae6e5445f181ea74fe06393b2e7
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-09-16 09:46:20 -0500

    reorganization of urls, implementation of url services in your app folder

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ProyectoWebApp/', include('ProyectoWebApp.urls')),
+    path('ProyectoWebApp/servicios/', include('servicios.urls')),
 ]
 
 if settings.DEBUG:
