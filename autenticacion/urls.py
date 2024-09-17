c30245e325c8e09a146a7f9096f072352c872b8e
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-09-17 11:34:36 -0500

    Session startup, session closing, data capture configuration and html template

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import LoginView
+from .views import LoginView, cerrar_sesion, logear
 
 urlpatterns = [
     path('', LoginView.as_view(), name="autenticacion"),
+    path('carrar_session',cerrar_sesion, name="cerrar_sesion"),
+    path('login', logear, name="login"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 class LoginView(View):
     def get(self, request):
@@ -15,3 +15,25 @@ class LoginView(View):
             login(request, usuario)
             return redirect('home')
         return render(request, 'registro/registro.html', {'form': form})  # Agregado para manejar el caso de formulario no válido
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('home')
+
+def logear(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            usuario = authenticate(username=nombre_usuario, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('home')
+            else:
+                return render(request, "login/login.html", {"form":form})
+        else:
+            return render(request, "login/login.html", {"form":form})
+    
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})  # Agregado para manejar el caso de formulario no válido
